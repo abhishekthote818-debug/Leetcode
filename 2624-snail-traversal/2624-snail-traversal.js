76b3744770b5b105588db55c54104eee91a5d3b6
abhishekthote818-debug <abhishekthote818@gmail.com>
2026-09-22 12:55:04 +0530

    Time: 152 ms (77.93%), Space: 68.1 MB (87.77%) - LeetHub

---
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -0,0 +1,29 @@
+Array.prototype.snail = function(rowsCount, colsCount) {
+    if (rowsCount * colsCount !== this.length) {
+        return [];
+    }
+
+    const result = Array.from(
+        { length: rowsCount },
+        () => Array(colsCount)
+    );
+
+    let index = 0;
+
+    for (let col = 0; col < colsCount; col++) {
+        if (col % 2 === 0) {
+            // Top to bottom
+            for (let row = 0; row < rowsCount; row++) {
+                result[row][col] = this[index++];
+            }
+        } else {
+            // Bottom to top
+            for (let row = rowsCount - 1; row >= 0; row--) {
+                result[row][col] = this[index++];
+            }
+        }
+    }
+
+    return result;
+};
+
